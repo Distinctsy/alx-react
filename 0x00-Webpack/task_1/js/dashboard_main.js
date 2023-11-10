e580ab8d8cf797e063f50601ede24ac31012ae7a
Distinctsy <oluwaseyiomi.com>
2023-11-10 12:28:12 +0000

    react

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,17 @@
-// Import jQuery (make sure you have jQuery installed or linked in your HTML file)
 import $ from 'jquery';
+import _ from 'lodash';
 
-// Create and add the specified elements to the HTML document
-$(document).ready(function() {
-  // Paragraph element: Holberton Dashboard
-  $('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  // Paragraph element: Dashboard data for the students
-  $('body').append('<p>Dashboard data for the students</p>');
+let conunt = 0;
 
-  // Button element with the text "Click here to get started"
-  $('body').append('<button id="startButton">Click here to get started</button>');
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-  // Add a paragraph element with the id 'count'
-  $('body').append('<p id="count"></p>');
-
-  // Add another paragraph: Copyright - Holberton School
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+$('button').on('click', _.debounce(updateCounter, 500));
